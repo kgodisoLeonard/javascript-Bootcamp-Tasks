3b409617bb96ef584d56e58c4f580e501c1cee59
kgodiso johannes Lebese <kgodisolebese@gmail.com>
2026-05-25 19:35:37 +0200

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,15 @@
+// Exercise 1: Variable Declaration
+let name = "KGODISO JOHANNES LEBESE";
+let age = 28;
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+// Exercise 2: Variable Reassignment
+let favoriteColour = "Blue";
+console.log("Favorite Colour:", favoriteColour);
+
+favoriteColour = "Green";
+console.log("New Favorite Colour:", favoriteColour);
